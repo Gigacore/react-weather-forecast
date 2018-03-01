038c7535ef2f28091985eaed1abd453b042577c9
Gigacore <santhoshinbang@gmail.com>
2018-03-01 09:25:04 +0530

    moved API key to constants

--- src/actions/weatherStation.js
+++ src/actions/weatherStation.js
@@ -1,12 +1,14 @@
 import { FETCH_DATA_FULFILLED, FETCH_DATA_REJECTED } from "../constants/ActionTypes";
+import { APP_ID } from "../constants/generalConstants";
+
 import axios from "axios";
 
 export const fetchData = (region) => (dispatch) => {
   
   const { latitude, longitude } = region || {};
 
-  const getDataByCity = `https://api.openweathermap.org/data/2.5/forecast?q=${region}&units=metric&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
-  const getDataByCoords = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+  const getDataByCity = `https://api.openweathermap.org/data/2.5/forecast?q=${region}&units=metric&appid=${APP_ID}`;
+  const getDataByCoords = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${APP_ID}`;
 
   let location = typeof(region) === "object" ? getDataByCoords : getDataByCity;
 
---
+++ src/constants/generalConstants.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+export const APP_ID = "fbf712a5a83d7305c3cda4ca8fe7ef29";
+
+/* eslint-enable */
