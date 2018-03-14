9127004d9832b25e509953d51934b1d88cfeb7a7
Gigacore <santhoshinbang@gmail.com>
2018-03-14 13:53:06 +0530

    hourly forecast updates

--- src/components/DetailedInfo.js
+++ src/components/DetailedInfo.js
@@ -1,17 +1,36 @@
 import React from "react";
 
 const DetailedInfo = ({ data }) => {
+
+  const displayMoreInfo = (item, i) => {
+    return (
+      <div className="hourly-info" key={i}>
+        <div className="hour-temperature">
+          {Math.round(item.main.temp) + "°C"}
+        </div>
+        <div className="hour-of-the-day">
+          {new Date(item.dt * 1000).getHours() + ":00"}
+        </div>
+      </div>
+    );
+  };
+
+  const getHour = (time) => {
+    return new Date(time).getHours();
+  };
+
+  const getDate = (date) => {
+    return date ? new Date(date).getDate() : new Date().getDate();
+  };
+
   return (
     <div className="hourly">
       {data.map((item, i) => (
-        <div className="hourly-info" key={i}>
-          <div className="hour-temperature">
-            {Math.round(item.main.temp) + "°C"}
-          </div>
-          <div className="hour-of-the-day">
-            {new Date(item.dt * 1000).getHours() + ":00"}
-          </div>
-        </div>
+        (getHour(item.dt * 1000) > new Date().getHours() && getDate(item.dt * 1000) === getDate()) ? (
+          displayMoreInfo(item, i)
+         ) : getHour(item.dt * 1000) < 23 ? (
+           displayMoreInfo(item, i)
+         ) : null
       ))}
     </div>
   );
