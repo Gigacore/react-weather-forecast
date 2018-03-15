ea466f29e92e44e13ed12a1f2bb424d1d1c4aafd
Gigacore <santhoshinbang@gmail.com>
2018-03-15 11:13:28 +0530

    cleanup

--- src/components/DetailedInfo.js
+++ src/components/DetailedInfo.js
@@ -2,21 +2,21 @@ import React from "react";
 
 const DetailedInfo = ({ data }) => {
 
+  const getHour = time => time ? new Date(time).getHours() : new Date().getHours();
+  const getDate = date => date ? new Date(date).getDate() : new Date().getDate();
+
   const displayMoreInfo = (item, i) => {
     return (
       <div className="hourly-info" key={i}>
         <div className="hour-temperature">
-          {Math.round(item.main.temp) + "°C"}
+          {`${Math.round(item.main.temp)}°C`}
         </div>
         <div className="hour-of-the-day">
-          {new Date(item.dt * 1000).getHours() + ":00"}
+          {`${getHour(item.dt * 1000)}:00`}
         </div>
       </div>
     );
   };
-
-  const getHour = time => time ? new Date(time).getHours() : new Date().getHours();
-  const getDate = date => date ? new Date(date).getDate() : new Date().getDate();
 
   return (
     <div className="hourly">
